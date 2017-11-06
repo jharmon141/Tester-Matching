2a6c7b8265c1b5d7906a8e50399aecf87aba1e00
jharmon141 <jeremy.l.harmon@gmail.com>
2017-11-05 18:34:33 -0600

    filter by devices and country

--- app.js
+++ app.js
@@ -1,9 +1,191 @@
-import csv from 'csv-to-json';
+import Prompt from 'prompt-checkbox';
+import Table from 'cli-table'; 
+import mysql from 'mysql';
+import 'console.table';
 
-const obj = { filename: './data/testers.csv' };
+var availableTesters = []; //[{testerId, firstName, lastName, lastLogin}]
+var availableDevices = []; // [{deviceId, description}]
+var availableCountries = []; //[countries]
 
-function writeJSON(err, json) {
-  console.log(json);
-};
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "test_match"
+});
 
-csv.parse(obj, writeJSON);
+const promptCountry = new Prompt ({
+  message: "Please choose a country or countries (For all countries select 'ALL'):",
+  name: "country",
+  choices: {
+    ALL: availableCountries 
+  }
+});
+
+const promptDevice = new Prompt ({
+  type: "checkbox",
+  message: "Choose the device(s) you would like to test:",
+  name: "devices",
+  choices: {
+    ALL: availableDevices
+  }
+});
+
+runProgram();
+
+async function runProgram() {
+  await setAvailableCountriesAndTesters();
+  await setAvailableDevices();
+  promptUser();
+}
+
+function promptUser() {
+  promptCountry.run()
+    .then(countryAnswer => {
+      promptDevice.run()
+        .then(deviceAnswer => {
+          searchForTesters(countryAnswer, deviceAnswer);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+async function searchForTesters(countries, devices) {
+  let filteredTesters;
+
+  if (devices[0] !== "ALL" && countries[0] !== "ALL") {
+    //filter testers by country
+    filteredTesters = filterTestersByCountry(countries, availableTesters);
+    //get filtered list of tests available devices
+    const testersDevices = await getTestersDevices(filteredTesters);
+    //filter out testers that do not have device
+    filteredTesters = filterTestersByDevices(devices, testersDevices, filteredTesters);
+  }
+  else if (devices[0] !== "ALL" && countries[0] === "ALL") {
+    filteredTesters = availableTesters;
+    const testersDevices = await getTestersDevices(filteredTesters);
+    filteredTesters = filterTestersByDevices(devices, testersDevices, filteredTesters);
+  }
+  else if (countries[0] !== "ALL" && devices[0] === "ALL") {
+    filteredTesters = filterTestersByCountry(countries, availableTesters);
+  }
+  else filteredTesters = availableTesters
+
+  // sortAndPrintTesters(filteredTesters);
+  // promptSearchAgain();
+
+  console.table(filteredTesters);
+  process.exit();
+}
+
+function filterTestersByDevices(devices, testersDevices, testers) {
+  let testersIDs = [];
+  let filteredTesters = [];
+  let deviceIDs = [];
+
+  //set device Ids by looking them up in availableDevices
+  devices.forEach(device => {
+    availableDevices.forEach(aDevice => {
+      if (device === aDevice.name) {
+        deviceIDs.push(aDevice.deviceId);
+      }
+    });
+  });
+
+  //get ids of only testers that have these devices
+  testersDevices.forEach(testerDevice => {
+    deviceIDs.forEach(deviceID => {
+      if (deviceID === testerDevice.deviceId && !testersIDs.includes(testerDevice.testerId)) {
+        testersIDs.push(testerDevice.testerId);
+      }
+    });
+  });
+
+  console.log(deviceIDs);
+  console.log(testersDevices);
+  //remove any testers that do not have all devices
+  testersIDs.forEach(tester => {
+    deviceIDs.forEach(device => {
+      if (!testersDevices.includes({ testerId: 1, deviceId: 1 })) {
+        testersIDs.splice(testersIDs.indexOf(tester), 1);
+      }
+    });
+  });
+
+
+  console.log(testersIDs);
+  //create array of filtered testers
+  testersIDs.forEach(id => {
+    testers.forEach(tester => {
+      if (id === tester.testerId) {
+        filteredTesters.push(tester);
+      }
+    });
+  });
+
+  return filteredTesters;
+}
+
+function filterTestersByCountry(countries, testers) {
+  let filteredTesters =[];
+
+  countries.forEach( country => {
+    testers.forEach(tester => {
+      if (tester.country === country && !filteredTesters.includes(tester)) {
+        filteredTesters.push(tester);
+      }
+    })
+  })
+  return filteredTesters;
+}
+
+function getTestersDevices(testers) {
+  let testerIDS = [];
+
+  testers.forEach((tester, i) => {
+    testerIDS.push(tester.testerId);
+  });
+
+  return new Promise ( (resolve, reject) => {
+    connection.query('SELECT * from tester_device WHERE testerId IN (' + testerIDS.join() + ')', (err, rows) => {
+      if (err) throw err;
+      resolve(rows);
+    });
+
+  });
+}
+
+
+function setAvailableCountriesAndTesters() {
+  return new Promise ( (resolve, reject) => {
+    connection.query('SELECT * from testers', (err,rows) => {
+      if (err) throw err;
+
+      resolve(rows.forEach(tester => {
+        availableTesters.push(tester);
+
+        if (!availableCountries.includes(tester.country)) {
+          availableCountries.push(tester.country);
+        }
+      }));
+    });
+  });
+}
+
+function setAvailableDevices() {
+  return new Promise ( (resolve, reject) => {
+    connection.query('SELECT * from devices', (err,rows) => {
+      if (err) throw err;
+
+      resolve(rows.forEach( device => {
+        availableDevices.push( {deviceId: device.deviceId, name: device.description});
+      }));
+    });
+  });
+}
